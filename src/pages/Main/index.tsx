8a95b617007f6166530d68a96619724172ae4ffe
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-01-18 21:44:45 -0400

    Create global styles

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,19 +1,3 @@
-import logo from '@assets/logo.svg';
-import styles from './styles.module.scss';
-
 export function Main() {
-  return (
-    <div className={styles.app}>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <h1 className={styles.title}>
-          Welcome to <code>React.js</code>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>src/App.tsx</code>
-        </p>
-      </header>
-    </div>
-  );
+  return <h1>Hello, World!</h1>;
 }
