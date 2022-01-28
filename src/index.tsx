f9fef66f1feadd8a8dede489ebcdd3a663807923
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-01-27 23:58:39 -0400

    Implement fake API server with miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,22 @@
 import { render } from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = `api`;
+
+    this.get(`transactions`, () => [
+      {
+        id: 1,
+        title: `Transaction 1`,
+        amount: 100,
+        type: `deposit`,
+        category: `Food`,
+        createAt: new Date(),
+      },
+    ]);
+  },
+});
 
 render(<App />, document.getElementById(`root`));
