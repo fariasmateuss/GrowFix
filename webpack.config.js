2631d7859cd8f4887d48404e8396ad6e525b65a2
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-08-23 23:59:13 -0400

    Add support for both JSX and TSX

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(j|t)sx$/,
+        test: /\.(js|ts|jsx|tsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
