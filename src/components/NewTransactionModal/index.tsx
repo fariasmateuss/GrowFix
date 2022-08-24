344af65dd3898b9dc87d799dcb012083b5219e26
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-08-23 21:58:21 -0400

    Implement NewTransactionModal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
 import Modal from 'react-modal';
 
 import close from '@assets/close.svg';
+import imcome from '@assets/income.svg';
+import outcome from '@assets/outcome.svg';
 
 import { NewTransationModalProps } from './types';
-
 import styles from './styles.module.scss';
 
 Modal.setAppElement(`#root`);
@@ -24,13 +25,23 @@ export function NewTransactionModal({
         onClick={onRequestClose}
         className={styles.modalClose}
       >
-        {close}
+        <img src={close} alt="Close" />
       </button>
 
       <div>
         <h2 className={styles.heading}>Register transation</h2>
         <input className={styles.modalInput} type="text" placeholder="Title" />
         <input className={styles.modalInput} type="text" placeholder="Value" />
+        <div className={styles.transationTypeContainer}>
+          <button className={styles.transationsTypeButton} type="button">
+            <img src={imcome} alt="Income" />
+            <span>Income</span>
+          </button>
+          <button className={styles.transationsTypeButton} type="button">
+            <img src={outcome} alt="Outcome" />
+            <span>Outcome</span>
+          </button>
+        </div>
         <input
           className={styles.modalInput}
           type="text"
