4f667ca58397f4e48dda6baf13e30d77514758bb
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-08-27 16:12:26 -0400

    Add entry module to index.html file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
   root: `src`,
   build: {
     outDir: `build`,
   },
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: `GrowFix: Financial Management`,
+        },
+      },
+    }),
+  ],
 });
